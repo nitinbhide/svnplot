3cb660cc9f02f0cd166c54df45d4219bef2233ec
Nitin Bhide <31431663+nitinbhide@users.noreply.github.com>
2009-09-02 13:06:08 +0000

    added __init__.py for eliminating the package installation error. Remove the 'setup not available' line from comment.

---
+++ src/svnplot/__init__.py
@@ -0,0 +1,6 @@
+'''
+Copyright (C) 2009 Nitin Bhide (nitinbhide@gmail.com)
+
+This module is part of SVNPlot (http://code.google.com/p/svnplot) and is released under
+the New BSD License: http://www.opensource.org/licenses/bsd-license.php
+'''
