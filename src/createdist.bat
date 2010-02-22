8b541dfe4e357770677874d1779c05a1d211a466
nitinbhide <nitinbhide@ce807b64-be9c-11dd-ab14-3be0eea8eb7e>
2010-02-22 12:52:42 +0000

    minor corrections.

--- src/createdist.bat
+++ src/createdist.bat
@@ -1,7 +1,7 @@
 @echo off
 REM Creating binrary and source distribution of SVNPlot
 echo "Creating SVNPlot source distribution in zip format"
-setup.py sdist --formats=zip
+python setup.py sdist --formats=zip
 echo "Creating Windows installer for SVNPlot"
-setup.py bdist_wininst
+python setup.py bdist_wininst
 
