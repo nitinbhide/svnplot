8e614060c12495a46c3edaf63ddc4ccdc23e8a1e
nitinbhide <nitinbhide@ce807b64-be9c-11dd-ab14-3be0eea8eb7e>
2011-11-14 05:58:43 +0000

    added a line to automatically delete minifest.

--- src/createdist.bat
+++ src/createdist.bat
@@ -1,6 +1,7 @@
 @echo off
 REM Creating binrary and source distribution of SVNPlot
 del /s /q build\*
+del MANIFEST
 echo "Creating SVNPlot source distribution in zip format"
 python setup.py sdist --formats=zip
 echo "Creating Windows installer for SVNPlot"
