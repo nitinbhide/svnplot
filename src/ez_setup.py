d9322207907aa08a9b19a042ee86c47f571f6000
nitinbhide <nitinbhide@ce807b64-be9c-11dd-ab14-3be0eea8eb7e>
2013-06-07 09:24:15 +0000

    Easy setup uses setuptools/distribute to download and install necessary dependencies.

---
+++ src/ez_setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+'''
+setup.py
+Copyright (C) 2009 Nitin Bhide (nitinbhide@gmail.com)
+
+This module is part of SVNPlot (http://code.google.com/p/svnplot) and is released under
+the New BSD License: http://www.opensource.org/licenses/bsd-license.php
+--------------------------------------------------------------------------------------
+Setup file for installing svnplot
+'''
+import sys
+
+import distribute_setup
+distribute_setup.use_setuptools()
+
+from setuptools import setup, find_packages
+
+setup(name='SVNPlot', version ='0.7.9',      
+      description='python module to generate graphs and statistics from Subversion repository data',
+      author='Nitin Bhide',
+      author_email='nitinbhide@gmail.com',
+      license = 'http://www.opensource.org/licenses/bsd-license.php',
+      url='http://code.google.com/p/svnplot',
+      install_requires=['numpy', 'matplotlib'],
+      dependency_links = [
+        "http://pysvn.tigris.org/servlets/ProjectDocumentList?folderID=1768"
+        ],
+      packages=['svnplot'],
+      package_dir = {'svnplot': 'svnplot'},
+      package_data= {'svnplot':['readme.txt', 'README', 'javascript/*.js', 'javascript/jqplot/*.*',
+                     'javascript/jqplot/plugins/*.js']},
+      scripts = ['svnlog2sqlite.py', 'svnplot.py', 'svnplotjs.py'],
+      exclude_package_data= { '': ['.svn/*']},      
+      classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Version Control"
+        ],
+      zip_safe=False,
+     )
