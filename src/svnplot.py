0080c4875e8c2afbdfc9156d31b5c090742c72e2
Nitin Bhide <31431663+nitinbhide@users.noreply.github.com>
2013-06-07 10:09:33 +0000

    modifications to copying the svnlog2sqlite.py and svnplot.py and svnplotjs.py to scripts folder.

---
+++ src/svnplot.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+'''
+svnplot.py
+Copyright (C) 2009 Nitin Bhide (nitinbhide@gmail.com)
+
+This module is part of SVNPlot (http://code.google.com/p/svnplot) and is released under
+the New BSD License: http://www.opensource.org/licenses/bsd-license.php
+--------------------------------------------------------------------------------------
+'''
+from svnplot.svnplot import *
+
+if( __name__ == "__main__"):
+    RunMain()
+    
