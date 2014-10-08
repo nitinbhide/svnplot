a5ef3cf7963672656d9a2fe86d9a74b55a214ea4
Nitin Bhide <31431663+nitinbhide@users.noreply.github.com>
2014-10-08 16:30:12 +0530

    fixed the d3.v3 install issue.

--- src/setup.py
+++ src/setup.py
@@ -10,7 +10,7 @@ Setup file for installing svnplot
 
 from distutils.core import setup
 
-setup(name='SVNPlot', version ='0.8.1',
+setup(name='SVNPlot', version ='0.8.2',
       description='python module to generate graphs and statistics from Subversion repository data',
       author='Nitin Bhide',
       author_email='nitinbhide@gmail.com',
@@ -20,7 +20,7 @@ setup(name='SVNPlot', version ='0.8.1',
       packages=['svnplot'],
       package_dir = {'svnplot': 'svnplot'},
       package_data= {'svnplot':['readme.txt', 'README', 'javascript/*.js', 'javascript/jqplot/*.*',
-                     'javascript/jqplot/plugins/*.js']},
+                     'javascript/jqplot/plugins/*.js', 'javascript/d3.v3/*.*']},
       scripts = ['svnlog2sqlite.py', 'svngraphs.py', 'svnplotjs.py'],
       
       classifiers=[
