69e2965c9a6ba781eb6c236fdba63dfc552e8fc6
nitinbhide <nitinbhide@thinkingcraftsman.in>
2016-03-02 16:15:53 +0530

    modifications to enable wheel creation. Also updated url to bitbucket url.

--- src/createdist.bat
+++ src/createdist.bat
@@ -5,6 +5,8 @@ del MANIFEST
 python setup.py clean
 echo "Creating SVNPlot source distribution in zip format"
 python setup.py sdist --formats=zip
-echo "Creating Windows installer for SVNPlot"
+echo Creating Windows installer for SVNPlot
 python setup.py bdist_wininst
+echo Creating Python Wheel
+python setup.py bdist_wheel
 REM python setup.py bdist_egg
