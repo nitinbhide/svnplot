c9de784d534bba68bf9fc8ea3ecda89778498da6
nitinbhide <nitinbhide@thinkingcraftsman.in>
2016-09-25 11:01:43 +0530

    added a paramer (python version) create distributions specific to python versions.

--- src/createdist.bat
+++ src/createdist.bat
@@ -1,12 +1,15 @@
 @echo off
 REM Creating binrary and source distribution of SVNPlot
+set PYTHON_VER=%1
 del /s /q build\*
 del MANIFEST
-python setup.py clean
+REM Setup distributions for Python 2
+echo Creating distributions for Python %PYTHON_VER%
+py -%PYTHON_VER% setup.py clean
 echo "Creating SVNPlot source distribution in zip format"
-python setup.py sdist --formats=zip
+py -%PYTHON_VER% setup.py sdist --formats=zip
 echo Creating Windows installer for SVNPlot
-python setup.py bdist_wininst
+py -%PYTHON_VER% setup.py bdist_wininst --target-version %PYTHON_VER%
 echo Creating Python Wheel
-python setup.py bdist_wheel
+py -%PYTHON_VER% setup.py bdist_wheel
 REM python setup.py bdist_egg
